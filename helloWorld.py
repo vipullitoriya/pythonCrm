1d8142456369c0e45d82198aab618c5aac7ec82a
Vipul Litoriya <vipullitoriya777@gmail.com>
2023-02-07 11:47:35 +0530

    Here Flat List Programe Modified

--- helloWorld.py
+++ helloWorld.py
@@ -1,1 +1,12 @@
+## FlatList
+ls = [[1,2,4,[5,[6,78],[35,[5,[6,78],8]]]]]
+lsNew = []
 
+def flatListHere(ls):
+    for i in ls:
+        if type(i) == list:
+            flatListHere(i)
+        else:
+            lsNew.append(i)
+flatListHere(ls)
+print(lsNew)
